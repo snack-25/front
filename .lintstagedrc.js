7f724f4b34ef22dacda6aa70ed465de854c6e3b9
ChoboDev <gealot@gmail.com>
2025-03-06 15:01:06 +0900

    ⬆️ chore(deps): TailwindCSS 버전 업데이트(4.0.9 -> 4.0.10)

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -3,6 +3,6 @@ import path from 'path';
 const buildEslintCommand = (filenames) =>
   `next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --file ')}`;
 
-module.exports = {
+export default {
   '*.{js,jsx,ts,tsx}': [buildEslintCommand],
 };
