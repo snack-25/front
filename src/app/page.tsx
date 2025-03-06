20dfadcb0e6984b40344d864de6d454aa7ec93c4
ChoboDev <gealot@gmail.com>
2025-03-06 18:15:24 +0900

    💄 chore(styles): CSS 포맷 정리 및 레이아웃 개선

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,10 @@ export default function Home() {
   return (
     <div className='flex flex-col items-center justify-center h-screen'>
       <span>안녕하세요, Snack25 입니다.</span>
-      <img src='/img/card/item-coke-zero.png' alt='코카콜라 제로' />
+      <img
+        src='/img/card/item-coke-zero.png'
+        alt='코카콜라 제로'
+      />
     </div>
   );
 }
