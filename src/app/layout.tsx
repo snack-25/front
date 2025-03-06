6faa1680eefe431af181d31e2f3caa243d1f6870
ChoboDev <gealot@gmail.com>
2025-03-06 18:14:54 +0900

    ✨ feat(styles): 폰트 및 레이아웃 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='ko'>
-      <body className={`${pretendard.variable} font-pretendard antialiased`}>
+      <body className={`${pretendard.variable} antialiased`}>
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
-    <>
+    <div className='flex flex-col items-center justify-center h-screen'>
       <span>안녕하세요, Snack25 입니다.</span>
-    </>
+      <img src='/img/card/item-coke-zero.png' alt='코카콜라 제로' />
+    </div>
   );
 }
