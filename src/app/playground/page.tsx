638b12e086eedfc9a85f6067cf57558ab9623b49
ChoboDev <gealot@gmail.com>
2025-03-07 13:50:34 +0900

    💄 style: body 최소 너비 설정 및 JSX 속성의 따옴표 통일

--- src/app/playground/page.tsx
+++ src/app/playground/page.tsx
@@ -1,7 +1,7 @@
 export default function PlayGround() {
   return (
-    <div className="text-white mb:bg-red-500 tb:bg-yellow-300 lt:bg-blue-500">
+    <div className='text-white mb:bg-red-500 tb:bg-yellow-300 lt:bg-blue-500'>
       브레이크 포인트 테스트용
     </div>
-  )
+  );
 }
