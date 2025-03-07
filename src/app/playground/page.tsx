082a3b7587779ecbc51fd40dc5a3660c00d762c1
just-codingbaby <gocks5393@naver.com>
2025-03-07 16:44:26 +0900

    💄 style: 커스터마이징 컬러, 폰트 크기 추가

--- src/app/playground/page.tsx
+++ src/app/playground/page.tsx
@@ -1,6 +1,6 @@
 export default function PlayGround() {
   return (
-    <div className='text-white mb:bg-red-500 tb:bg-yellow-300 lt:bg-blue-500'>
+    <div className='text-white mb:bg-red-500 tb:bg-yellow-300 lt:bg-primary-400 text-xl'>
       브레이크 포인트 테스트용
     </div>
   );
