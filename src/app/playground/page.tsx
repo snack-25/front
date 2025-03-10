1eb0ce0b4837f933ee1f9f9ecb57573051ef34b7
just-codingbaby <gocks5393@naver.com>
2025-03-10 18:29:46 +0900

    ✨ feat(shared): add shared-gnb

--- src/app/playground/page.tsx
+++ src/app/playground/page.tsx
@@ -1,7 +1,12 @@
+import Header from '@/components/gnb/Header';
+
 export default function PlayGround() {
   return (
-    <div className='text-white mb:bg-red-500 tb:bg-yellow-300 lt:bg-primary-400 text-xl'>
-      브레이크 포인트 테스트용
-    </div>
+    <>
+      <Header />
+      <div className='text-white text-xl bg-black'>
+        브레이크 포인트 테스트용
+      </div>
+    </>
   );
 }
