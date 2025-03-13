197a65ebacc25916adb7320cc3f5cb4540a2be49
ChoboDev <gealot@gmail.com>
2025-03-13 18:23:05 +0900

    ✨ feat(product-list): SortDropDown을 Suspense로 감싸 로딩 상태 처리 추가

--- src/app/productList/page.tsx
+++ src/app/productList/page.tsx
@@ -11,7 +11,9 @@ export default function ProductList() {
         <TabMenu />
       </Suspense>
       <div className='w-full h-[98px] max-lt:h-[68px] px-[120px] max-lt:px-6 flex  items-center justify-end'>
-        <SortDropDown />
+        <Suspense fallback={<div>로딩중...</div>}>
+          <SortDropDown />
+        </Suspense>
       </div>
 
       <CardList />
