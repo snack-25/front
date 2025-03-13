3dc990048c89d4d2c73803211fa9cc92b5f74f2e
xcjnzvc <rkdenrjd@gmail.com>
2025-03-13 14:39:33 +0900

    ✨ : 회원가입 페이지 ui 완성 및 버튼 컴포넌트 수정

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -2,8 +2,45 @@
 
 import { Input } from '@/components/ui/Input_auth';
 import { Button } from '@/components/ui/Button';
+import { useState } from 'react';
+import { signupApi } from '@/app/api/auth/api';
 
 export default function Signup() {
+  const [form, setForm] = useState({
+    name: '',
+    email: '',
+    password: '',
+    validatePassword: '',
+    company: '',
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setForm((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleClick = () => {
+    const { name, email, password, validatePassword, company } = form;
+
+    if (!name || !email || !password || !validatePassword || !company) {
+      alert('모든 항목을 입력해주세요!');
+      return;
+    }
+    console.log(form);
+    if (password !== validatePassword) {
+      alert('비밀번호를 다시 확인해주세요');
+      return;
+    }
+
+    signupApi(form).then((res) => {
+      console.log('res', res);
+    });
+  };
+
+  const isFormValid = Object.values(form).every((value) => value.length > 0);
+
   return (
     <div className=''>
       <div className='py-[80px] tb:pb-[100px] px-[24px] tb:max-w-[640px] m-auto flex flex-col '>
@@ -19,42 +56,42 @@ export default function Signup() {
           <Input
             name='name'
             placeholder='이름을 입력해주세요'
-            // onChange={handleChange}
+            onChange={handleChange}
           >
             이름(기업 담당자)
           </Input>
           <Input
             name='email'
             placeholder='이메일을 입력해주세요'
-            // onChange={handleChange}
+            onChange={handleChange}
           >
             이메일
           </Input>
           <Input
             name='password'
             placeholder='비밀번호를 입력해주세요'
-            // onChange={handleChange}
+            onChange={handleChange}
           >
             비밀번호
           </Input>
           <Input
             name='validatePassword'
             placeholder='비밀번호를 다시 한 번 입력해주세요'
-            // onChange={handleChange}
+            onChange={handleChange}
           >
             비밀번호 확인
           </Input>
           <Input
-            name='company'
+            name='companyId'
             placeholder='회사명을 입력해주세요'
-            // onChange={handleChange}
+            onChange={handleChange}
           >
             회사명
           </Input>
           <Button
             className='mt-[16px] tb:mt-[40px]'
-            filled='gray'
-            // onClick={handleClick}
+            filled={isFormValid ? 'orange' : 'gray'}
+            onClick={handleClick}
           >
             시작하기
           </Button>
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,41 +1,47 @@
-import * as React from "react";
-import { Slot } from "@radix-ui/react-slot";
-import { cva, type VariantProps } from "class-variance-authority";
-import { cn } from "@/lib/utils";
+import * as React from 'react';
+import { Slot } from '@radix-ui/react-slot';
+import { cva, type VariantProps } from 'class-variance-authority';
+import { cn } from '@/lib/utils';
 
 const buttonVariants = cva(
-  "rounded-[16px] h-[54px] tb:h-[64px] text-[16px] tb:text-[20px] font-[600] inline-flex items-center justify-center gap-2 ",
+  'inline-flex items-center justify-center gap-2 inline-flex  whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 ',
   {
     variants: {
+      variant: {
+        default: 'bg-primary text-primary-foreground hover:bg-primary/90',
+        destructive:
+          'bg-destructive text-destructive-foreground hover:bg-destructive/90',
+        outline:
+          'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
+        secondary:
+          'bg-secondary text-secondary-foreground hover:bg-secondary/80',
+        ghost: 'hover:bg-accent hover:text-accent-foreground',
+        link: 'text-primary underline-offset-4 hover:underline',
+      },
       filled: {
-        gray: "bg-[#E0E0E0] text-white hover:bg-gray-400",
-        orange: "bg-[#F97B22] text-white hover:bg-orange-500",
-        light: "bg-[#FDF0DF] text-[#F97B22] hover:bg-orange-100",
+        gray: 'rounded-[16px] h-[54px] tb:h-[64px] text-[16px] tb:text-[20px] font-[600] bg-[#E0E0E0] text-white hover:bg-gray-400',
+        orange:
+          'rounded-[16px] h-[54px] tb:h-[64px] text-[16px] tb:text-[20px] font-[600] bg-[#F97B22] text-white hover:bg-orange-500',
+        light:
+          'rounded-[16px] h-[54px] tb:h-[64px] text-[16px] tb:text-[20px] font-[600] bg-[#FDF0DF] text-[#F97B22] hover:bg-orange-100',
         textgray:
-          "bg-[#EFEFEF] rounded-[16px] text-[#999999] hover:bg-gray-400 hover:text-white",
+          'rounded-[16px] h-[54px] tb:h-[64px] text-[16px] tb:text-[20px] font-[600] bg-[#EFEFEF] rounded-[16px] text-[#999999] hover:bg-gray-400 hover:text-white',
       },
       outlined: {
-        none: "",
-        gray: "border border-[#E0E0E0] bg-transparent text-[#E0E0E0] pointer-events-none",
+        gray: 'rounded-[16px] h-[54px] tb:h-[64px] text-[16px] tb:text-[20px] font-[600] border border-[#E0E0E0] bg-transparent text-[#E0E0E0] pointer-events-none',
         orange:
-          "border border-[#F97B22] bg-transparent text-[#F97B22] hover:bg-orange-100",
+          'rounded-[16px] h-[54px] tb:h-[64px] text-[16px] tb:text-[20px] font-[600] border border-[#F97B22] bg-transparent text-[#F97B22] hover:bg-orange-100',
         light:
-          "border border-[#F97B22] bg-[#FBF8F4] text-[#F97B22] hover:bg-orange-50",
+          'rounded-[16px] h-[54px] tb:h-[64px] text-[16px] tb:text-[20px] font-[600] border border-[#F97B22] bg-[#FBF8F4] text-[#F97B22] hover:bg-orange-50',
       },
     },
-    defaultVariants: {
-      filled: "gray",
-      outlined: "none",
-    },
-  }
+  },
 );
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
   asChild?: boolean;
-  filled?: "gray" | "orange" | "light" | "textgray";
-  outlined?: "gray" | "orange" | "light" | "none";
   rounded?: string;
   height?: string;
   font?: string;
@@ -56,9 +62,9 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
       asChild = false,
       ...props
     },
-    ref
+    ref,
   ) => {
-    const Comp = asChild ? Slot : "button";
+    const Comp = asChild ? Slot : 'button';
     return (
       <Comp
         className={cn(
@@ -69,7 +75,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
           font,
           height,
           rounded,
-          className
+          className,
         )}
         onClick={onClick}
         ref={ref}
@@ -77,9 +83,9 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
         style={width ? { width } : undefined}
       />
     );
-  }
+  },
 );
 
-Button.displayName = "Button";
+Button.displayName = 'Button';
 
 export { Button, buttonVariants };
