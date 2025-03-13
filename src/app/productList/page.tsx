301c738b6a7ceb79b864a3efd0636b1cd3936c89
ChoboDev <gealot@gmail.com>
2025-03-13 18:21:55 +0900

    ✨ feat(product-list): TabMenu를 Suspense로 감싸 로딩 상태 처리 추가

--- src/app/productList/page.tsx
+++ src/app/productList/page.tsx
@@ -1,3 +1,5 @@
+import { Suspense } from 'react';
+
 import TabMenu from '@/components/gnb/TabMenu';
 import CardList from '@/components/productList/CardList';
 import { SortDropDown } from '@/components/productList/SortDropDown';
@@ -5,7 +7,9 @@ import { SortDropDown } from '@/components/productList/SortDropDown';
 export default function ProductList() {
   return (
     <>
-      <TabMenu />
+      <Suspense fallback={<div>로딩중...</div>}>
+        <TabMenu />
+      </Suspense>
       <div className='w-full h-[98px] max-lt:h-[68px] px-[120px] max-lt:px-6 flex  items-center justify-end'>
         <SortDropDown />
       </div>
