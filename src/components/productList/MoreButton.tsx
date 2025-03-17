aed7c990ea50c702fba5dcf11212eb2decae5214
just-codingbaby <gocks5393@naver.com>
2025-03-17 16:13:53 +0900

    ✨ feat(ProductList): 더보기 버튼 컴포넌트 추가

---
+++ src/components/productList/MoreButton.tsx
@@ -0,0 +1,34 @@
+'use client';
+import { cn } from '@/lib/utils';
+import { Button } from '../ui/Button';
+import { ChevronDown } from 'lucide-react';
+import { useRef } from 'react';
+
+interface IProps {
+  className: string;
+  onClick: () => void;
+}
+
+export default function MoreButton({ className }: IProps) {
+  const iconRef = useRef<SVGSVGElement | null>(null);
+
+  return (
+    <>
+      <div className={className}>
+        <Button
+          onClick={() => iconRef.current?.classList.toggle('rotate-180')}
+          variant='outline'
+          className='lt:w-[640px] lt:h-16 max-lt:w-[327px] max-lt:h-[54px]  text-primary-400 border-1 border-primary-400 bg-gray-50 flex gap-2 cursor-pointer [&_svg]:w-6 [&_svg]:h-6'
+        >
+          <span className='lt:text-xl font-semibold max-lt:text-lg'>
+            더보기
+          </span>
+          <ChevronDown
+            ref={iconRef}
+            className='transform transition-transform duration-300'
+          />
+        </Button>
+      </div>
+    </>
+  );
+}
