85c2e5e7567cc8fd1bd2b416202dc2f5efb4498e
just-codingbaby <gocks5393@naver.com>
2025-03-31 15:18:43 +0900

    🐛 fix(PlayGround): 린트에러 처리

--- src/app/playground/page.tsx
+++ src/app/playground/page.tsx
@@ -4,15 +4,7 @@ import { useCustomToast } from '@/components/ui/Toast/Toast';
 export default function PlayGround() {
   return (
     <>
-      <div>
-        <button
-          onClick={() => {
-            useCustomToast({ label: '예산이 변경되었습니다.' });
-          }}
-        >
-          토스트 띄우기
-        </button>
-      </div>
+      <div></div>
     </>
   );
 }
