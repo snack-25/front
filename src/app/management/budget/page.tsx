f19986d9261f8528bb5fb05bbb71783fff7a9629
xcjnzvc <rkdenrjd@gmail.com>
2025-04-01 23:06:13 +0900

    🐛 fix(Budget): 코드 주석 추가 및 리턴 위치 정리

--- src/app/management/budget/page.tsx
+++ src/app/management/budget/page.tsx
@@ -184,7 +184,7 @@ export default function Budget() {
       console.error('Submit failed:', error);
     }
   };
-
+  //리턴
   return (
     <div className='py-[80px] tb:pb-[100px] px-[24px] tb:max-w-[640px] m-auto flex flex-col'>
       <div className='pr-[10px]'>
