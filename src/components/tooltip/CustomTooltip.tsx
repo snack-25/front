6e23b7772fca32af9fe5706fa8de21ababf27c48
dani784601 <139238536+dani784601@users.noreply.github.com>
2025-04-07 18:42:22 +0900

    ➖fix: Remove Radix Tooltip dependencies
    Signed-off-by: dani784601 <139238536+dani784601@users.noreply.github.com>

--- src/components/tooltip/CustomTooltip.tsx
+++ src/components/tooltip/CustomTooltip.tsx
@@ -1,20 +1,8 @@
-import * as React from 'react';
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipPortal,
-  TooltipTrigger,
-  TooltipProvider,
-  TooltipArrow,
-} from '@radix-ui/react-tooltip';
-import { Button } from '@/components/ui/Button';
-
 interface CustomTooltipProps {
   content: string;
-  placement?: 'top' | 'right' | 'bottom' | 'left';
 }
 
-const CustomTooltip = ({ content, placement = 'top' }: CustomTooltipProps) => {
+const CustomTooltip = ({ content }: CustomTooltipProps) => {
   return (
     <div className='grid place-items-center'>
       <div className='bg-primary-400 text-white font-bold text-[26px] rounded-full py-5 px-8 whitespace-nowrap'>
@@ -32,24 +20,6 @@ const CustomTooltip = ({ content, placement = 'top' }: CustomTooltipProps) => {
         </svg>
       </span>
     </div>
-    // <TooltipProvider>
-    //   <Tooltip>
-    //     <TooltipTrigger asChild>
-    //       <Button className='aspect-square bg-amber-200'>{content}</Button>
-    //     </TooltipTrigger>
-    //     <TooltipContent
-    //       forceMount
-    //       className='bg-primary-400 text-white px-5 py-8 rounded-full font-bold text-[26px]'
-    //     >
-    //       {content}
-    //       <TooltipArrow
-    //         width={32}
-    //         height={26}
-    //         className='fill-primary-400'
-    //       />
-    //     </TooltipContent>
-    //   </Tooltip>
-    // </TooltipProvider>
   );
 };
 
