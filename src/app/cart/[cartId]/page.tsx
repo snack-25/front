b29a6fb828ff1ab817f99f3395bb456e1c3f0b2b
soohwanpak <qoiuytnghks4@gmail.com>
2025-04-08 20:57:41 +0900

    :tada: feat: 장바구니 반응형 구현

--- src/app/cart/[cartId]/page.tsx
+++ src/app/cart/[cartId]/page.tsx
@@ -226,13 +226,13 @@ export default function CartsPage() {
           <div className='flex justify-between px-4 lg:px-6 py-4 bg-[#F9F6F1] text-sm text-gray-600 border-t'>
             <div className='w-full lg:w-[310px] flex justify-between gap-2'>
               <button
-                className='w-1/2 h-[50px] px-[18px] py-[12px] rounded-full border border-[#E0E0E0] text-[#1F1F1F]'
+                className='w-1/2 h-[50px] px-[18px] py-[12px] rounded-full border border-[#E0E0E0] text-[#1F1F1F] cursor-pointer'
                 onClick={handleDeleteAll}
               >
                 전체 상품 삭제
               </button>
               <button
-                className='w-1/2 h-[50px] px-[18px] py-[12px] rounded-full border border-[#E0E0E0] text-[#1F1F1F]'
+                className='w-1/2 h-[50px] px-[18px] py-[12px] rounded-full border border-[#E0E0E0] text-[#1F1F1F] cursor-pointer'
                 onClick={handleDelete}
               >
                 선택 상품 삭제
